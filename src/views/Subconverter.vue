f0aa02fa53a087c89634afb9d90f6f5b9297178c
周润发 <shaoyouvip@gmail.com>
2026-04-19 05:18:05 +0800

    同步cm edt 规则

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -347,32 +347,40 @@ export default {
             label: "CM规则",
             options: [
               {
-                label: "CM_Online 默认版 识别港美地区(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini"
+                label: "默认版 识别港美地区 CM_Online",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online.ini"
               },
               {
-                label: "CM_Online_MultiCountry 识别港美地区 负载均衡(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"
+                label: "精简版 自动测速、故障转移、负载均衡 识别CloudFlareCDN CM_Online_Mini_MultiMode_CF",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Mini_MultiMode_CF.ini"
               },
               {
-                label: "CM_Online_MultiCountry_CF 识别港美地区、CloudFlareCDN 负载均衡 Worker节点专用(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry_CF.ini"
+                label: "精简版 不带自动测速(有效减少-1情况) 识别CloudFlareCDN CM_Online_Mini_NoAuto_CF",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Mini_NoAuto_CF.ini"
               },
               {
-                label: "CM_Online_Full 识别多地区分组(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full.ini"
+                label: "识别港美地区 负载均衡 CM_Online_MultiCountry",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"
               },
               {
-                label: "CM_Online_Full_CF 识别多地区、CloudFlareCDN 分组 Worker节点专用(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_CF.ini"
+                label: "识别港美地区、CloudFlareCDN 负载均衡 CF节点专用 CM_Online_MultiCountry_CF",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_MultiCountry_CF.ini"
               },
               {
-                label: "CM_Online_Full_MultiMode 识别多地区 负载均衡(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"
+                label: "识别多地区分组 CM_Online_Full",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full.ini"
               },
               {
-                label: "CM_Online_Full_MultiMode_CF 识别多地区、CloudFlareCDN 负载均衡 Worker节点专用(与Github同步)",
-                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode_CF.ini"
+                label: "识别多地区、CloudFlareCDN 分组 CF节点专用 CM_Online_Full_CF",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_CF.ini"
+              },
+              {
+                label: "识别多地区 负载均衡 CM_Online_Full_MultiMode",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"
+              },
+              {
+                label: "识别多地区、CloudFlareCDN 负载均衡 CF节点专用 CM_Online_Full_MultiMode_CF",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_MultiMode_CF.ini"
               }
             ]
           },
